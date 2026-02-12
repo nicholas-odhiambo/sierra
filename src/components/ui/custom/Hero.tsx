9a01b1b78ad1808fd5a1829db0b1ca3cb059558e
Nicholas Odhiambo <odhiambonicolas.no@gmail.com>
2026-02-12 17:09:04 +0300

    ui changes on hero section

--- src/components/ui/custom/Hero.tsx
+++ src/components/ui/custom/Hero.tsx
@@ -14,18 +14,27 @@ const Hero = () => {
       <Container maxW="6xl" fluid pt="20">
         <Grid templateColumns="repeat(4, 1fr)" gap="40px">
           <GridItem colSpan={2}>
-            <Text fontWeight="bold" fontSize="3xl" color="#ffffff">
+            <Text
+              as="h1"
+              fontWeight="medium"
+              fontSize={{ base: "2xl", md: "4xl", lg: "6xl" }}
+              color="#ffffff"
+              lineHeight={1.1}
+            >
               {" "}
-              Automate live chat without coding{" "}
+              Automate workflow without coding{" "}
             </Text>
-            <Text fontWeight="lighter" fontSize="2xl" color="#ffffff">
+            <Text as="p" fontWeight="lighter" fontSize="2xl" color="#ffffff">
               Experience the future of customer interactions today with our
               intuitive, AI-powered solution.
             </Text>
-            <Button>View Pricing plans </Button>
+            <Button px="15" marginTop="10">
+              View Pricing plans{" "}
+            </Button>
+            <Box></Box>
           </GridItem>
-          <GridItem colSpan={1}>
-            <Image height="" src="/images/sierra-chatbot-4.png" />
+          <GridItem colSpan={2}>
+            <Image minH="45 vh" src="/images/sierra-chatbot-4.png" />
           </GridItem>
         </Grid>
       </Container>
